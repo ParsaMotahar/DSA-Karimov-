ab1c0d9655faee8ec67b69f46c25d1e22b2a0773
Parsa Motahar <parsa.mota@gmail.com>
2023-11-03 03:53:25 -0500

    NA

---
+++ Technical Questions/SumOfDiagElements.py
@@ -0,0 +1,18 @@
+'''Given 2D list calculate the sum of diagonal elements.
+
+Example
+
+    myList2D= [[1,2,3],[4,5,6],[7,8,9]] 
+     
+    diagonal_sum(myList2D) # 15'''
+
+import numpy as np
+arr = np.array([[1,2,3],[4,5,6], [7,8,9]])
+def diagonal_sum(arr):
+    c = 0
+    sum = 0
+    for r in range(len(arr)):
+        sum += arr[r][c]
+        c += 1
+    return sum
+print(diagonal_sum(arr))
