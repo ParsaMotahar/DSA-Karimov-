85f5bd1b4c391fd3062654d1214282b5a4941ea4
Parsa Motahar <parsa.mota@gmail.com>
2023-11-07 23:14:53 -0600

    n

---
+++ Technical Questions/Dictionaries/CountWordFrequency.py
@@ -0,0 +1,23 @@
+'''Count Word Frequency
+
+Define a function to count the frequency of words in a given list of words.
+
+Example:
+
+    words = ['apple', 'orange', 'banana', 'apple', 'orange', 'apple'] 
+    count_word_frequency(words) 
+
+Output:
+
+     {'apple': 3, 'orange': 2, 'banana': 1}'''
+
+words = ['apple', 'orange', 'banana', 'apple', 'orange', 'apple'] 
+
+def count_word_frequency(words):
+    counts = {}
+    for word in words:
+        counts.setdefault(word, 0)
+    for word in words:
+        counts[word] = counts.get(word, 0) + 1
+    return counts
+print(count_word_frequency(words))
